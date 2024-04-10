032db39e8b85be5060ef07385eef361406590383
William <wsimplicio123@gmail.com>
2024-04-10 19:13:44 -0300

    final-commit

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 let toggles = document.querySelectorAll('.toggle');
-let contentDiv = document.querySelectorAll('.content');
+let contents = document.querySelectorAll('.content');
 let icon = document.querySelectorAll('.icon-plus');
 
 for(let i = 0; i < toggles.length; i++){
-    toggles[i].addEventListener('click', () =>{
-        if(contentDiv[i].style.display === 'none'){
-            contentDiv[i].style.display = 'block';
-            icon[i].src = "/images/icon-minus.svg";
+    toggles[i].addEventListener('click', function(){
+        if(contents[i].style.display === 'none'){
+            contents[i].style.display = 'block';
+            icon[i].src = '/images/icon-minus.svg';
         } else {
-            contentDiv[i].style.display = 'none';
-            icon[i].src = "/images/icon-plus.svg"
-        };
-
-        for(let y = 0; y < contentDiv.length; y++){
-            if(y!=i){
-                contentDiv[y].style.display = 'none';
-                icon[y].src = "/images/icon-plus.svg"
+            contents[i].style.display = 'none';
+            icon[i].src = '/images/icon-plus.svg';
+        }
+        for(let c = 0; c < contents.length; c++){
+            if(c!=i){
+                contents[c].style.display = 'none';
+                icon[c].src = '/images/icon-plus.svg';
             }
-        };
-    });
-};
+        }
+    })
+}
+
+
 
 
 
