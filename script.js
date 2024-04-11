af99761eee926100632d86fd0b15c3901ee41745
William <wsimplicio123@gmail.com>
2024-04-11 09:55:17 -0300

    final-commit

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ let icon = document.querySelectorAll('.icon-plus');
 
 for(let i = 0; i < toggles.length; i++){
     toggles[i].addEventListener('click', function(){
-        if(contents[i].style.display === 'none'){
-            contents[i].style.display = 'block';
+        if(contents[i].classList.contains('hidden')){
+            contents[i].classList.remove('hidden')
             icon[i].src = '/images/icon-minus.svg';
         } else {
-            contents[i].style.display = 'none';
+            contents[i].classList.add('hidden');
             icon[i].src = '/images/icon-plus.svg';
         }
+
         for(let c = 0; c < contents.length; c++){
             if(c!=i){
-                contents[c].style.display = 'none';
+                contents[c].classList.add('hidden');
                 icon[c].src = '/images/icon-plus.svg';
             }
         }
